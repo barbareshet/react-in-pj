34312a0d38300a88430bfec3063b9883cb158b3e
barbareshet <barbareshet@gmail.com>
2020-06-16 11:57:22 +0300

    first commit
    basic project structure
    lesson 8 solution - Reset counter on 10

---
+++ counter/src/counter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Counter(props) {
+  const [count, setCount] = useState(0);
+  const { delta, isTen } = props;
+
+  function inc() {
+    setCount(oldValue => oldValue + delta);
+  }
+  function resetOnTen(){
+    if (count == 10 ){
+      reset();
+    }
+  }
+  function reset(){
+    setCount(1);
+  }
+
+  return (
+    <div>
+      <>
+      <pre>{JSON.stringify(props)}</pre>
+      <p>
+        I was clicked {count >= 10 ? reset() : count} times        
+        <button onClick={inc}>Click Me</button>
+        <button onClick={reset}>Reset</button>
+      </p>
+      </>
+    </div>
+  );
+}
---
+++ counter/src/main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
+import Counter from './counter';
+
+const App = () => {
+  const [delta, setDelta] = useState(1);
+
+  function handleChangeDelta(e) {
+    const newValue = Number(e.target.value);
+    setDelta(newValue);
+  }
+
+  return (
+      <div>
+        <label>
+          Increase By:
+          <input type="number" value={delta} onChange={handleChangeDelta} />
+        </label>
+        <Counter delta={delta} isTen={false}/>
+        <Counter delta={delta + 1}/>
+      </div>
+  )
+};
+
+
+// main.js
+const root = document.querySelector('main');
+ReactDOM.render(<App />, root);
